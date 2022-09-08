1990b5cd18969d7485303dde20a4fe3e8a692843
Killua <ibrahimtarekeslim@gmail.com>
2022-09-08 12:40:34 +0300

    test

--- pages/dashboard/entries/index.vue
+++ pages/dashboard/entries/index.vue
@@ -7,11 +7,19 @@
         </v-btn>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col v-for="(curr, index) in currencies" :key="curr.id">
+    <v-row class="lg5-custome-row" justify="center">
+      <v-col
+        v-for="(curr, index) in currencies"
+        :key="curr.id"
+        cols="12"
+        sm="6"
+        md="4"
+        xs="12"
+      >
         <v-card class="black-font">
           <v-row no-gutters class="black-font">
             <v-col
+              style="color: #000 !important"
               cols="12"
               class="text-center justify-center d-flex pb-0 pt-2 black-font"
             >
@@ -19,36 +27,46 @@
                 <v-img width="30" height="30" src="/entriesBoxIcon.png" />
               </div>
 
-              <div
-                class="font-wigth-black"
-                style="color: rgba(139, 139, 139, 0.93)"
-              >
-                {{ "صندوق" }} {{ curr.name }}
-              </div>
+              <div class="font-wigth-black">{{ "صندوق" }} {{ curr.name }}</div>
 
               <!-- <InputField class="pt-3 pl-3 pr-3"></InputField> -->
             </v-col>
             <v-col class="text-center pb-0 black-font" cols="12">
               <div class="black-font" style="color: rgba(139, 139, 139, 0.93)">
-                $  400 
+                $ 400
               </div>
             </v-col>
             <v-col class="text-center pb-0 black-font" cols="12">
-              <div class="pa-3 black-font"><input class="border" /></div>
+              <div class="pa-3 black-font">
+                <input type="number" class="border" />
+              </div>
             </v-col>
           </v-row>
-          <v-card-text>
-            <v-row v-for="t in trans[index]" :key="t" justify="start" class="black-font">
-              <v-divider></v-divider>
-              <div class="pt-1" style="color: rgba(139, 139, 139, 0.93)"> {{ t }} </div>
+          <v-card-text class="pa-0 black-font">
+            <v-row
+              v-for="t in trans[index]"
+              :key="t"
+              justify="start"
+              no-gutters
+              class="black-font"
+            >
+              <!-- <v-divider></v-divider> -->
+              <v-col
+                class="pt-2 pb-1"
+                style="border-top: solid grey 1px; width: 100%"
+              >
+                &nbsp; {{ t | money
+                 }} 
+              </v-col>
             </v-row>
           </v-card-text>
 
           <v-btn
             depressed
             style="background-color: #83b97b"
-            class="white--text"
+            class="white--text mt-1 black-font"
             block
+            height="60"
           >
             {{ $t("balanced") }}
           </v-btn>
@@ -68,7 +86,7 @@ export default {
       item: {
         convertToUSD: null,
       },
-      currencies: [
+      currenciess: [
         { id: 1, name: "dollar", values: { sale: 1, buy: 1 } },
         { id: 2, name: "denar", values: { sale: 0.708, buy: 0.706 } },
         { id: 3, name: "shekel", values: { sale: 3.32, buy: 3.3 } },
@@ -77,6 +95,7 @@ export default {
       ],
       trans: [
         [3000, 3000, 3000],
+        [3000, 3000, 3000, 3000, 3000],
         [3000, 3000, 3000, 3000, 3000],
         [3000, 3000, 3000, 3000, 3000],
         [3000, 3000, 3000, 3000, 3000],
@@ -101,16 +120,40 @@ export default {
   created() {
     this.$store.dispatch("currency/index");
   },
-  components: { Card },
+  filters: {
+    money(value) {
+      if (value) {
+        return value.toLocaleString(undefined, { minimumFractionDigits: 2 });
+      }
+    },
+  },
 };
 </script>
 
 <style>
 .border {
   border: 2px solid #d6d6d6;
+  border-radius: 6px;
 }
 .black-font {
   font-size: large;
-  font-weight: bold !important;
+  /* font-weight: bold !important; */
+  color: #000 !important;
+}
+
+@media (min-width: 1264px) {
+  .lg5-custome-row > div {
+    width: 20% !important;
+    max-width: 20% !important;
+    flex-basis: 20% !important;
+  }
+}
+input[type=number]::-webkit-inner-spin-button, 
+input[type=number]::-webkit-outer-spin-button { 
+  -webkit-appearance: none; 
+}
+
+input[type=number] {
+  -moz-appearance: textfield;
 }
 </style>
